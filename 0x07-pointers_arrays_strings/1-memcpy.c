d052d5b9d2a8e7f2035d8d9ee802798f05ea89f7
Aduseikay8096 <aduseikay8096@gmail.com>
2023-01-03 17:04:48 -0800

    task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,6 @@
  * @dest: pointer to dest str
  * @src: source to be copied from
  * @n: num bytes to copy from src
- *
  * Return: pointer to beginning of memory area dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)

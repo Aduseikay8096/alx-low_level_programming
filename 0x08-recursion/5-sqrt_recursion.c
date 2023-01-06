b4e83b8ed2aee19eea4b424c44631309cc0c1933
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-01-05 14:44:07 -1200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
+
 /**
- * wildcmp - Entry Point
- * @s1: input
- * @s2: input
- * Return: 0
+ * sqrt_check - Find the square root
+ * @n: Number to find square root of
+ * @c: The number checked to see if it's the sqrt of n
+ *
+ * Return: The square root value
  */
-int wildcmp(char *s1, char *s2)
+int sqrt_check(int n, int c)
 {
-	return (0);
+	if (c * c == n)
+	{
+		return (c);
+	}
+	else if (c >= n / 2)
+		return (-1);
+	return (sqrt_check(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - Find the square root of given number
+ * @n: Number given to find root of
+ *
+ * Return: The square root or -1 if there is no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	return (sqrt_check(n, 1));
 }

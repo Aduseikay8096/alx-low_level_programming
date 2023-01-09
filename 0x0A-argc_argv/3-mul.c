2f2b418b8db75de03c3ec5fc86d9bb1a2cbe65e1
Aduseikay8096 <aduseikay8096@gmail.com>
2023-01-09 13:50:26 -0800

    task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * main - function to print multiplication result
  * @argc: number of arguments
  * @argv: arguments supplied
  * Return: always 1 if less than two arg var supplied otherwise 0.
  */
+
 int main(int argc, char **argv)
 {
 	int a = 0, b = 0;
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
 /**
  * main - function to add variables
  * @argc: number of arguments
  * @argv: arguments being passed
  * Return: Always 0 or 1
  */
+
 int main(int argc, char **argv)
 {
 	int a, b, addition;

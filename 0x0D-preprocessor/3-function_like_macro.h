d75da1a0a3fe2a3cf6cf9fb33587f709d3c9dfb4
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-01-17 10:16:14 -1200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-
-
-#endif /* FUNCTION_LIKE_MACRO */
-=======
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
-
 #define ABS(x) (((x) < 0) ? -(x) : (x))
 #endif
->>>>>>> 2ad4856dcf8bd0f82c3fe68c4aa7c784ea128e30

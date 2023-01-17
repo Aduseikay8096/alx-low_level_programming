c455ce6e891b7fe7af940727b3969716cf777590
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-01-17 09:04:40 -1200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include "1-pi.h"
-#include "1-pi.h"
 #include <stdio.h>
 
 /**
@@ -9,11 +8,6 @@
  */
 int main(void)
 {
-	float a;
-	float r;
-
-	r = 98;
-	a = PI * r * r;
-	printf("%.3f\n", a);
+	printf("%s\n", __FILE__);
 	return (0);
 }

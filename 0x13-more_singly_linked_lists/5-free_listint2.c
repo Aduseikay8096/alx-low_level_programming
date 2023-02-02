e20d09dba5f32f732254142490c82fb4f1d6c344
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-02-01 12:55:37 -1200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,26 @@
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint2 - frees a list
- * @head: head node
- * Return: void
+ * free_listint2 - function with one argument
+ * @head: pointer to head pointer of list
+ *
+ * Description: frees a list
+ * Return: na
  */
 void free_listint2(listint_t **head)
 {
-	if (!head || !*head)
+	listint_t *traverse;
+
+	if (head == NULL)
+	{
 		return;
+	}
+
 	while (*head)
 	{
-		listint_t *tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		traverse = (*head)->next;
+		free(*head);
+		*head = traverse;
 	}
-	*head = 0;
+	*head = NULL;
 }

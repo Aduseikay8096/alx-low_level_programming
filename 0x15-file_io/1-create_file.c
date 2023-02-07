77cdfaad159670853ae2f492ee9e33ef2a6258f0
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-02-06 14:41:13 -1200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,41 @@
 #include "main.h"
+
 /**
- * create_file - Entry Point
- * @filename: file name
- * @text_content: null terminated string to write
- * Return: 1
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ *
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int count = 0;
+	int fd = 0;
+	int output = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		while (text_content[count] != '\0')
+			count++;
 	}
 	else
 	{
-		write(file, text_content, i);
+		close(fd);
+		return (1);
 	}
 
-	close(file);
+	output = write(fd, text_content, count);
+	if (output == -1 || output != count)
+		return (-1);
+
+	close(fd);
 	return (1);
 }

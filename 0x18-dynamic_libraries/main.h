8d963ad4881b5d2494e3f39888bd25274ce8c9d5
Adusei Kwadwo Yeboah <97838009+Aduseikay8096@users.noreply.github.com>
2023-03-21 02:25:56 +0000

    Add files via upload

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef _holberton_h_
-#define _holberton_h_
+#ifndef DYNAMIC_LIB
+#define DYNAMIC_LIB
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _holberton_h_ */
+#endif /* DYNAMIC_LIB */
